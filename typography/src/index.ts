df205475371668135540f284fd53da269b3f7963
$hibaKing <78498397+tylerbishopdev@users.noreply.github.com>
2023-04-05 17:29:10 -0700

    try

--- typography/src/index.ts
+++ typography/src/index.ts
@@ -1,6 +1,9 @@
 export const typography = {
   fontFamily: {
     code: ['Menlo', 'ui-monospace', 'SFMono-Regular', 'Monaco', 'monospace'],
+    sans: ['Inter',
+      'ui-sans-serif',
+      'system-ui']
   },
   fontWeight: {
     custom: '600',
